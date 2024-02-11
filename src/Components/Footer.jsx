076aebfd301dbe40ecbdc90eb753a01f7975a09f
murugan <150892750+murugan21-bcs@users.noreply.github.com>
2024-02-11 19:00:02 +0530

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+
+
+function Footer() {
+return (
+<footer className="bg-dark text-light py-4">
+<div className="container">
+<div className="row justify-content-center align-items-center">
+<div className="col-md-6">
+<p className="mb-0">
+© 2024 All rights reserved By <span className="font-weight-bold">MURUGAN</span>.
+</p>
+</div>
+</div>
+</div>
+</footer>
+);
+}
+
+export default Footer;
